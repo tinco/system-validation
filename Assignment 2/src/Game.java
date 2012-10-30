553013fa9c9712518ebac78a295ae40fab8f5d0c
Daniel Moscoviter <daniel.moscoviter@gmail.com>
2012-10-30 22:53:59 +0100

    reduce error amount to 4

--- Assignment 2/src/Game.java
+++ Assignment 2/src/Game.java
@@ -43,6 +43,8 @@ final class Game {
     @*/
   boolean wonGame () {
     boolean result = true;
+
+    //@ loop_invariant x >= 0;
     for (int x = 0; x < board.xSize; x++) {
       if (!checkWonRow (board.items[x])) {
         result = false;
